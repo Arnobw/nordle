3062bfe182d6534a23244648cbd0463e3851e57c
Arnobw <Arnobw@hotmail.com>
2022-02-07 15:12:50 +0100

    klaar misschien

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-function handleCopyTextFromArea() {
-    const area = document.querySelector('#clipboard-area')
-    area.select();
+// document.getElementById('btn').addEventListener('click', handleCopyTextFromArea());
+
+// function handleCopyTextFromArea() {
+//     const area = document.querySelector('#clipboard-area')
+//     area.select();
+//     document.execCommand('copy')
+//   }
+
+// function copyToClipboard(element) {
+//     var $temp = $("<input>");
+//     $("body").append($temp);
+//     $temp.val($(element).text()).select();
+//     document.execCommand("copy");
+//     $temp.remove();
+//   }
+  
+
+function copyToClipboard(element) {
+    var text = $(element).clone().find('br').prepend('\r\n').end().text()
+    element = $('<textarea>').appendTo('body').val(text).select()
     document.execCommand('copy')
+    element.remove()
   }
